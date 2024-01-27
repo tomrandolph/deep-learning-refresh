e638848fbbb3096ce19bde5d97f512ba9cc66c8c
Tom Randolph <tomrandolph@Toms-MacBook-Pro.local>
2024-01-27 11:02:46 -0600

    Data loaded and displaying

---
+++ mnist_classifier_fc.py
@@ -0,0 +1,16 @@
+import torch
+import torchvision
+import matplotlib.pyplot as plt
+import numpy as np
+
+mnist_dataset = torchvision.datasets.MNIST(root='./datasets', download=True, train=True,
+                          transform=torchvision.transforms.Compose([torchvision.transforms.ToTensor()]),
+)
+
+idx = np.random.randint(0, len(mnist_dataset))
+
+digit, label = mnist_dataset[idx]
+
+plt.imshow(np.squeeze(digit))
+plt.title(label)
+plt.show()
